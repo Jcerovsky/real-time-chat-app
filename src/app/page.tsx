6d8717b5b0dfa912013299da10b3ef9f4af684b6
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-10-18 16:05:54 +1030

    add sign up form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import Input from "@/app/components/Input";
-import LoginPage from "@/app/components/LoginPage";
-
 export default function Home() {
-  return <LoginPage />;
+  return <h1>home</h1>;
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -9,19 +9,15 @@ function Page() {
       className="w-full sm:w-3/4 sm:ml-auto sm:mr-auto px-6 py-10 rounded-md bg-white dark:bg-primary-dark
     max-w-[38rem]"
     >
-      <h1 className="text-2xl text-center">
-        Welcome to your favourite chat app!
-      </h1>
-      <p className="mb-5 opacity-50 text-sm text-center">
-        Please log in or sign up below
-      </p>
+      <h1 className="text-2xl text-center mb-5">Create Account</h1>
       <Input placeholder="Username" />
-      <Input placeholder="Password " />
-      <Button style="mt-5">Log in</Button>
+      <Input placeholder="Password" />
+      <Input placeholder="Confirm your password" />
+      <Button style="mt-5">Sign up</Button>
       <div className=" mt-5 flex ">
-        <p className="opacity-40">{"Don't have an account?"}</p>
+        <p className="opacity-40">{"Already have an account?"}</p>
         <span className="ml-1 hover:text-blue-800 hover:underline ">
-          <Link href={"/signup"}>Sign up</Link>
+          <Link href={"/login"}>Log in</Link>
         </span>
       </div>
     </form>
