f2eca77a05cdcc33c9545bad47f519a3b65d2067
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-10-18 12:19:40 +1030

    add context

---
+++ src/app/Context/Context.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React, { createContext, ReactNode, useState } from "react";
+
+interface ContextProps {
+  theme: "light" | "dark";
+  setState: (state: Partial<ContextProps>) => void;
+}
+
+export const Context = createContext<ContextProps | null>(null);
+
+function ContextProvider({ children }: { children: ReactNode }) {
+  const [state, setState] = useState({
+    theme: "light",
+    setState: () => {},
+  });
+  return (
+    <Context.Provider value={{ theme: state.theme, setState }}>
+      {children}
+    </Context.Provider>
+  );
+}
+
+export default ContextProvider;
