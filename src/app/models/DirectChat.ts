52abfaaf1632afa973c680bfb8f52a118f0e361d
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-10-24 14:19:37 +1030

    create a direct chat schema

---
+++ src/app/models/DirectChat.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const chatSchema = new mongoose.Schema({
+  user1ID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  user2ID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  messages: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  ],
+});
+
+const DirectChat = mongoose.model("Chat", chatSchema, "Chat");
