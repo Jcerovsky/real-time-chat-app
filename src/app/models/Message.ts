75c2ebaea0783f2eda1f94e2c146c3a49fe00d22
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-10-24 14:11:25 +1030

    create a Message schema

---
+++ src/app/models/Message.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  fromUserID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  toUserID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Message = mongoose.model("Message", messageSchema, "Message");
