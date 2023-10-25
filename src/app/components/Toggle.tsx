40fde2622688f5c5655cd3bb557f96ce6146870b
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-10-25 11:09:48 +1030

    style toggle

--- src/app/components/Toggle.tsx
+++ src/app/components/Toggle.tsx
@@ -19,24 +19,27 @@ function Toggle() {
   }, [theme]);
 
   return (
-    <label className="relative inline-flex items-center cursor-pointer">
-      <input
-        type="checkbox"
-        value=""
-        className="sr-only peer"
-        checked={theme === "dark"}
-        onChange={() =>
-          setState({ theme: theme === "dark" ? "light" : "dark" })
-        }
-      />
-      <div
-        className="w-9 h-5 bg-gray-600 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300
-            dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full
-            peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px]
-            after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all
-            dark:border-gray-600 peer-checked:bg-blue-600 duration-300 border border-black"
-      />
-      <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300" />
+    <label className="flex items-center cursor-pointer">
+      <div className="relative">
+        <input
+          type="checkbox"
+          className="sr-only"
+          checked={theme === "dark"}
+          onChange={() =>
+            setState({ theme: theme === "dark" ? "light" : "dark" })
+          }
+        />
+        <div
+          className={`block w-10 h-6 rounded-full transition-colors duration-300 border border-neutral-700 ${
+            theme === "dark" ? "bg-neutral-600" : "bg-gray-400"
+          }`}
+        ></div>
+        <div
+          className={`dot absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition-transform duration-300 ${
+            theme === "dark" && "transform translate-x-full bg-gray-600"
+          }`}
+        ></div>
+      </div>
     </label>
   );
 }
