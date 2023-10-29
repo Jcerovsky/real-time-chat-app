e3f9016f7cdf2d305e5082003049afb81df5ba2f
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-10-29 20:52:53 +1030

    add message input styling

--- src/app/components/MessageInput.tsx
+++ src/app/components/MessageInput.tsx
@@ -13,14 +13,14 @@ function MessageInput({ sendMessage, state, setState }: I) {
     <form className="flex mt-4" onSubmit={sendMessage}>
       <input
         type="text"
-        className="py-2 px-4 w-full flex-grow rounded-md placeholder:ml-2 placeholder:font-light bg-gray-100 truncate
-            hover:bg-gray-200 duration-300 dark:bg-gray-700 dark:text-zinc-50 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]"
+        className="py-1 px-4 w-full rounded-md placeholder:ml-2 placeholder:font-light bg-gray-100 truncate
+            hover:bg-gray-200 duration-300 dark:bg-gray-700 dark:text-zinc-50 shadow text-sm sm:text-sm"
         placeholder="Type your message..."
         value={state.sentMessage}
         onChange={(e) => setState({ sentMessage: e.target.value })}
       />
       <Button
-        style="w-1/4 text-xs sm:text-sm text-center ml-2 rounded-lg flex justify-center md:whitespace-nowrap"
+        style="w-1/4 text-xs sm:text-sm text-left ml-2 rounded-lg flex justify-center md:whitespace-nowrap"
         isDisabled={!state.sentMessage}
         text={"Start typing..."}
       >
