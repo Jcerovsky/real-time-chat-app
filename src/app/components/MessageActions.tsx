fba6f419cf1c1ecd271fad6959a3ef76af52ab77
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-10-31 20:21:14 +1030

    change MessageActions styling

--- src/app/components/MessageActions.tsx
+++ src/app/components/MessageActions.tsx
@@ -13,20 +13,20 @@ function MessageActions({ isVisible, handleDelete }: I) {
   return (
     <div
       className={`absolute top-8 right-0 z-50 p-2 rounded-lg bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 
-      text-white shadow-md ${isVisible && "hidden"}`}
+      text-white shadow-md w-36 text-center ${isVisible && "hidden"}`}
     >
       <ul className="space-y-2">
         <li
-          className="cursor-pointer hover:bg-blue-700 p-1 rounded-md transition-colors duration-300"
+          className="cursor-pointer hover:bg-blue-700 p-1 rounded-md transition-colors duration-300 border-b-2"
           onClick={handleDelete}
         >
           Delete
         </li>
-        <li className="cursor-pointer hover:bg-blue-700 p-1 rounded-md transition-colors duration-300">
+        <li className="cursor-pointer hover:bg-blue-700 p-1 rounded-md transition-colors duration-300 border-b-2">
           Edit
         </li>
         <li
-          className="cursor-pointer hover:bg-blue-700 p-1 rounded-md transition-colors duration-300"
+          className="cursor-pointer hover:bg-blue-700 p-1 rounded-md transition-colors duration-300 border-b-2"
           onClick={handleCopyText}
         >
           Copy Text
