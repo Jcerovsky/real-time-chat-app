27cd0f699f51d3efd3094492c80db78b353c0c54
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-11-05 17:46:35 +1030

    time function changes

--- src/app/utils/calculateWhenMessageSent.ts
+++ src/app/utils/calculateWhenMessageSent.ts
@@ -1,14 +1,15 @@
 const calculateWhenMessageSent = (timeSent: Date) => {
   const date = new Date(timeSent);
 
-  return date.toLocaleDateString("en-AU", {
-    day: "2-digit",
-    month: "long",
-    year: "numeric",
-    hour: "numeric",
-    minute: "2-digit",
-    hour12: false,
-  });
+  return date
+    .toLocaleDateString("en-AU", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+      hour: "numeric",
+      minute: "2-digit",
+    })
+    .replace(",", " at ");
 };
 
 export default calculateWhenMessageSent;
