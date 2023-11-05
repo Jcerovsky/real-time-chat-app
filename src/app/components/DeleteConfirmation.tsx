31ee55e63e1471d37a080e2df88e5f21032e5fd4
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-11-05 13:38:08 +1030

    add responsive design to chat options

--- src/app/components/DeleteConfirmation.tsx
+++ src/app/components/DeleteConfirmation.tsx
@@ -13,22 +13,23 @@ function DeleteConfirmation({
 }: I) {
   return (
     <div
-      className=" absolute rounded-md p-6 bg-zinc-50 dark:bg-gray-800 flex flex-col right-10 w-[20rem] top-36"
+      className=" absolute right-0 sm:right-20 top-[14rem] sm:top-28 rounded-md p-6 bg-zinc-50 dark:bg-gray-800 flex flex-col  w-[13rem]
+      sm:w-[20rem]"
       ref={deleteConfirmationRef}
     >
-      <p>
+      <p className="text-xs sm:text-sm">
         Are you sure you want to delete your chat with {user}? This action is
         permanent and can not be reversed.
       </p>
-      <div className="flex justify-between gap-2 mt-4">
+      <div className="flex justify-between gap-2 mt-4 text-sm">
         <button
-          className="cursor-pointer bg-gray-500 hover:bg-gray-600 dark:hover:bg-gray-800 text-zinc-50 rounded-md py-2 px-4"
+          className="cursor-pointer bg-gray-500 hover:bg-gray-600 dark:hover:bg-gray-800 text-zinc-50 rounded-md  py-2 px-4"
           onClick={() => confirmDeletion("cancel")}
         >
           Cancel
         </button>
         <button
-          className="cursor-pointer bg-red-600 hover:bg-red-800 text-zinc-50 rounded-md py-2 px-4"
+          className="cursor-pointer bg-red-600 hover:bg-red-800 text-zinc-50 rounded-md  py-2 px-4"
           onClick={() => confirmDeletion("confirm")}
         >
           Confirm
