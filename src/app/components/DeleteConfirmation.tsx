fc6ee96c35918de0057cd202da32a84d090489ce
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-11-08 09:29:16 +1030

    fix delete confirmation positioning on small screens

--- src/app/components/DeleteConfirmation.tsx
+++ src/app/components/DeleteConfirmation.tsx
@@ -13,7 +13,7 @@ function DeleteConfirmation({
 }: I) {
   return (
     <div
-      className=" absolute right-0 sm:right-20 top-[14rem] sm:top-28 rounded-md p-6 bg-gray-200 dark:bg-gray-900 dark:text-zinc-100
+      className=" absolute right-[10%] sm:right-20 top-[14rem] sm:top-28 rounded-md p-6 bg-gray-200 dark:bg-gray-900 dark:text-zinc-100
        flex flex-col w-[13rem] sm:w-[20rem] z-30"
       ref={deleteConfirmationRef}
     >
