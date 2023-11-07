f95c6c57ada18e173d5d43c73c9bffb340eaf7b1
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-11-07 13:44:32 +1030

    fix signout api route to handle req and res correctly

--- src/app/api/users/signout/route.ts
+++ src/app/api/users/signout/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(res: NextResponse) {
+export async function POST(req: NextRequest, res: NextResponse) {
   await res.cookies.delete("token");
   return NextResponse.json(
     { message: "Successfully signed out" },
